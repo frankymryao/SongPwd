936c38b5e21be7079cb09ce7f9e53c1fa358b55b
franky <frankymryao@hotmail.com>
2011-06-19 23:34:03 +0800

    move getUsrData() to a single file

--- usrOperate.py
+++ usrOperate.py
@@ -41,12 +41,4 @@ def register():
 def login():
     usrName = raw_input('your login name: ')
 
-def getUsrData():
-    file_object = open('usrData')
-    try:
-        usrData = file_object.read().replace('\n', '').split('#')
-    finally:
-        file_object.close()
-    usrData = usrData[0:len(usrData) - 1]
-    print(usrData)
     

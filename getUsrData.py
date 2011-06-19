557d2e934efccd1b118e739d2d22a64fe4150ca2
franky <frankymryao@hotmail.com>
2011-06-19 23:23:46 +0800

    change the return value from list to a tuple of list containing 'name, times, pwd'

--- getUsrData.py
+++ getUsrData.py
@@ -2,7 +2,9 @@ def getUsrData():
     file_object = open('usrData')
     try:
         usrData = file_object.read().replace('\n', '').split('#')
+        name = usrData[0]
+        times = usrData[1]
     finally:
         file_object.close()
-    usrData = usrData[0:len(usrData) - 1]
-    print(usrData)
+    pwd = usrData[2:len(usrData) - 1]
+    return((name, times, pwd))

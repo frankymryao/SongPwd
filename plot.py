feaa4a61f8957a537cd50aa460d79fada730b35d
franky <frankymryao@hotmail.com>
2011-06-20 16:12:30 +0800

    adjust plot method to use modifible size

--- plot.py
+++ plot.py
@@ -5,15 +5,40 @@ from string import *
 import os
 
 theme.get_options()
-can = canvas.default_canvas()
-size = (300, 200)
-def plotDraw(data):
-    min, max = simpleSort(data)
-    drawArea = area.T(size = size, legend = None,
-            y_range = (min * 0.9, max * 1.1),
+
+class canAttr:
+    __width = 0
+    __height = 0
+    __y_low = 0
+    __y_high = 0
+    def __init__(self, size_x, size_y, y_range_low, y_range_high):
+        self.__width = size_x
+        self.__height = size_y
+        self.__y_low = y_range_low
+        self.__y_high = y_range_high
+    def getWidth(self):
+        return(self.__width)
+    def getHeight(self):
+        return(self.__height)
+    def getYLow(self):
+        return(self.__y_low)
+    def getYHigh(self):
+        return(self.__y_high)
+
+
+
+def plotDraw(can, canAttr, m_data):
+    min, max = simpleSort(m_data)
+    width = canAttr.getWidth()
+    height = canAttr.getHeight()
+    y_low = canAttr.getYLow()
+    y_high = canAttr.getYHigh()
+    drawArea = area.T(size = (width, height), 
+            legend = None,
+            y_range = (y_low, y_high),
             x_axis = axis.X(format = "%d", label = "times"),
             y_axis = axis.Y(format = "%d", label = "intervals(ns)"))
-    drawArea.add_plot(line_plot.T(data = data))
+    drawArea.add_plot(line_plot.T(data = m_data))
     drawArea.draw()
    # can.show(drawArea.x_pos(4), drawArea.y_pos(min), "/a45{}Intrvals")
 
@@ -31,7 +56,11 @@ def addSN(SN_data):
         plotData.append([i + 1, atof(SN_data[i])])
     return(plotData)
 if __name__ == '__main__':
+    can = canvas.default_canvas()
     name, times, pwd = getUsrData()
     pData = addSN(pwd)
-    tData = [ [1, 9], [2, 1], [3, 0], [4, 0], [5, 0] , [6, 0] ]
-    plotDraw(pData)
+    x = canAttr(300, 200, 300000, 900000)
+    print(x.getWidth(), x.getHeight())
+    tData = [ [1, 300000], [2, 400000], [3, 500000] ]
+    plotDraw(can, x, pData)
+    plotDraw(can, x, tData)

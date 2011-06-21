b273dfa28a21f5706e7f02674e081d459c079401
franky <frankymryao@hotmail.com>
2011-06-21 15:04:02 +0800

    add Test<T> mode and adjust whole process:

--- SongPwd.py
+++ SongPwd.py
@@ -7,16 +7,30 @@ import os
 
 x = raw_input('Login<L> or Register<R>?')
 if x == 'L':
-    lib_name, lib_count, lib_pwd = getUsrData()
-    input_pwd, input_count = getPwd()
-    print(lib_count, input_count)
-    if input_count == lib_count:
-        dualPlot(input_pwd, lib_pwd)
+    usrName = raw_input('your name:')
+    usrDict = getUsrData('usrData')
+    libPwd = usrDict.get(usrName)
+    if libPwd == None:
+        print('no usr')
+    libCount, libPwd = map2pwd(libPwd)
+    print(libPwd)
+    inputPwd, inputCount = getPwd()#get inputPwd
+    print(inputPwd)
+    if inputCount == libCount:
+        print('same count')
+        putPwd('cmp1', usrName, libCount, libPwd)
+        putPwd('cmp2', usrName, inputCount, inputPwd)
+        #use pwdCompare to outpu diagram
     else:
         print('wrong count')
 elif x == 'R':
     register()
 elif x == 'C':
     os.popen('rm usrData')
+    os.popen('rm cmp1')
+    os.popen('rm cmp2')
     print('usrData CLEARED!')
+elif x == 'T':
+    file_object = open('usrData')
+    print(file_object.readlines())
 else: print('wrong')
